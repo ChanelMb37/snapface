27beb8a301951d72f066ce7891f21652841d59b1
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-08-01 21:49:06 +0200

    creer propre class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
   
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'mon meilleur ami',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+    this.myOtherSnap = new FaceSnap(
+      'Montagne',
+      'mon meilleur voyage',
+      'https://cdn.pixabay.com/photo/2022/07/25/10/19/mountain-7343375_960_720.jpg',
+      new Date(),
+      0
+    );
+    this.myLastSnap = new FaceSnap(
+      'Dog',
+      'mon meilleur chien',
+      'https://cdn.pixabay.com/photo/2022/07/18/19/57/dog-7330712_960_720.jpg',
+      new Date(),
+      0,
+    );
+  }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,30 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
+@Input() faceSnap!: FaceSnap;
+
   // Pour "promettre" à TypeScript qu'on va les initialiser, on peut ajouter un bang  !  à chaque propriété :
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
   buttonText!: string;
   
   // On va  initialiser les quatre propriétés dans la méthode  ngOnInit()
   ngOnInit() {
-    this.title = 'Archibald';
-    this.description = 'mon meilleur ami';
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
     this.buttonText = "Oh Snap!"
   }
 
   onSnap() {
     if (this.buttonText === 'Oh Snap!') {
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.buttonText = 'Oops, unSnap';
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.buttonText = 'Oh Snap';
     }
     
