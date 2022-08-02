c8849c0e01bf3fee99b24469e84d4cc15e96bee5
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-08-02 11:00:21 +0200

    add directives ngIf et ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,33 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
-  
+  faceSnaps!: FaceSnap[];  
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'mon meilleur ami',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
-    this.myOtherSnap = new FaceSnap(
-      'Montagne',
-      'mon meilleur voyage',
-      'https://cdn.pixabay.com/photo/2022/07/25/10/19/mountain-7343375_960_720.jpg',
-      new Date(),
-      0
-    );
-    this.myLastSnap = new FaceSnap(
-      'Dog',
-      'mon meilleur chien',
-      'https://cdn.pixabay.com/photo/2022/07/18/19/57/dog-7330712_960_720.jpg',
-      new Date(),
-      0,
-    );
+    this.faceSnaps = [
+    {
+      title: 'Archibald',
+      description: 'mon meilleur ami',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'Paris'
+    },
+    {
+      title: 'Montagne',
+      description: 'mon meilleur voyage',
+      imageUrl: 'https://cdn.pixabay.com/photo/2022/07/25/10/19/mountain-7343375_960_720.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'montagne'
+    },
+     {
+      title:'Dog',
+      description: 'mon meilleur chien',
+      imageUrl: 'https://cdn.pixabay.com/photo/2022/07/18/19/57/dog-7330712_960_720.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+    }
+    ];
   }
 }
