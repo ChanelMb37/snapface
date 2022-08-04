c5f60f6a7f850115c347bb417045205cfb8420ea
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-08-04 11:32:13 +0200

    creer landing-page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 // tableau routes va lier les routes d'application (les différentes URL) aux components correspondants.
 // Pour enregistrer ces routes dans application, il faut les passer au routeur en passant un objet de configuration au décorateur  @NgModule()  de AppRoutingModule  
 const routes: Routes = [
-   {path: 'facesnaps', component: FaceSnapListComponent}
+   {path: 'facesnaps', component: FaceSnapListComponent},
+   {path: '', component: LandingPageComponent}
 ];
 
 // Ici, vous dites à Angular que les routes de ce fichier seront les routes à la racine de votre application, et vous réexportez le routeur configuré. 
