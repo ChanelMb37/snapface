c9c2cdec2ef6208e0d6387658f8bc5c597e97592
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-08-05 14:33:30 +0200

    strategies unsubscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable } from 'rxjs';
-import { map, filter, tap } from 'rxjs/operators';
+import { map, filter, tap, take, mergeMap, concatMap, delay, switchMap } from 'rxjs/operators';
 
 
 @Component({
@@ -11,6 +11,9 @@ import { map, filter, tap } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   
   interval$!: Observable<string>;
+
+  // redTrainsCalled = 0;
+  // yellowTrainsCalled = 0;
 
   ngOnInit() {
     this.interval$ = interval(1000).pipe(
@@ -26,4 +29,29 @@ export class AppComponent implements OnInit {
   logger(text: string): void {
     console.log(`Log: ${text}`);
   }
+
+  // opérateurs haut niveau
+  // interval(500).pipe(
+  //     take(10),
+  //     map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+  //     tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+  //     mergeMap(color => this.getTrainObservable$(color)),
+  //     tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+  //   ).subscribe();
+  // }
+
+  // getTrainObservable$(color: 'rouge' | 'jaune') {
+  //   const isRedTrain = color === 'rouge';
+  //   isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+  //   const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+  //   console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+  //   return of({ color, trainIndex }).pipe(
+  //     delay(isRedTrain ? 5000 : 6000)
+  //   );
+  // }
+
+  // translateColor(color: 'rouge' | 'jaune') {
+  //   return color === 'rouge' ? 'red' : 'yellow';
+  // }
 }
+
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,20 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject } from 'rxjs';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { tap, take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
-  faceSnaps!: FaceSnap[];  
+export class FaceSnapListComponent implements OnInit, OnDestroy {
+  faceSnaps!: FaceSnap[]; 
+  
+  // Un Subject est un Observable que vous pouvez faire émettre à la demande. 
+  // Vous allez donc créer un Subject appelé  destroy$  qui émettra une seule fois, au moment de la destruction du component.
+  private destroy$!: Subject<boolean>;
 
 constructor(private faceSnapsService: FaceSnapsService ) {
 
 }
   ngOnInit():void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    interval(1000).pipe(
+      // take() prend un nombre comme argument, et complète l'Observable quand il a émis ce nombre de valeurs.
+      // take(3),
+      tap(console.log),
+      // opérateur takeUntil dit à l'Observable  interval  de continuer à émettre tant que  destroy$  n'a pas émis, 
+      // mais dès que  destroy$  émet, de compléter l'Observable.
+      takeUntil(this.destroy$)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 
 }
