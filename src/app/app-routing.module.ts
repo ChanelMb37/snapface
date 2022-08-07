87328ae5d038d1085593714d6f2742d28e9d82b3
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-08-07 17:33:40 +0200

    creer la formulaire et validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { NewFaceSnapComponent } from "./new-face-snap/new-face-snap.component";
 import { SingleFaceSnapComponent } from "./single-face-snap/single-face-snap.component";
 
 // tableau routes va lier les routes d'application (les différentes URL) aux components correspondants.
@@ -9,6 +10,7 @@ import { SingleFaceSnapComponent } from "./single-face-snap/single-face-snap.com
 const routes: Routes = [
    {path: 'facesnaps/:id', component: SingleFaceSnapComponent},
    {path: 'facesnaps', component: FaceSnapListComponent},
+   {path: 'create', component: NewFaceSnapComponent},
    {path: '', component: LandingPageComponent}
 ];
 
