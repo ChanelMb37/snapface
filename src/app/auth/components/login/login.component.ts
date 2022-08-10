2068e938a90c5de65842e3345067cdc68f61d924
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-08-10 11:34:51 +0200

    Implémentez LoginComponent

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service.ts.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
+    this.auth.login();
+    this.router.navigateByUrl('/facesnaps');
   }
 
 }
